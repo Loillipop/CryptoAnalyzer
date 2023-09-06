03ad753049ba692e3f4ec2ff0f2f8e34d18305b7
lolipop <spoon9004@gmail.com>
2023-09-07 00:55:54 +0200

    add new files classes

---
+++ src/DecryptEncrypt/Encryptor.java
@@ -0,0 +1,4 @@
+package DecryptEncrypt;
+
+public class Encryptor {
+}
---
+++ src/UserInterface/GoBabyGo.java
@@ -0,0 +1,7 @@
+package UserInterface;
+
+public class GoBabyGo {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
